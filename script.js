80c5bf9d758f1e0c659cad8b672a5f077c0d3b6e
agungleksono <agungleksono99@gmail.com>
2022-07-05 00:21:32 +0700

    fixing add data and create detail data

--- script.js
+++ script.js
@@ -11,8 +11,28 @@ fetch('https://povid.herokuapp.com/api/v1/records')
 		});
 		const recordContainer = document.querySelector('.record-container');
 		recordContainer.innerHTML = table_records;
+
+		// show detail record
+		const modalDetailBtn = document.querySelectorAll('.modal-detail-btn');
+		modalDetailBtn.forEach(btn => {
+			btn.addEventListener('click', function() {
+				const recordid = this.dataset.recordid;
+				console.log(recordid);
+				fetch('https://povid.herokuapp.com/api/v1/records/' + recordid)
+					.then(response => response.json())
+					.then(record => {
+						const detail = record.data;
+						const recordDetail = showRecordDetail(detail);
+						const modalBody = document.querySelector('.modal-detail-body');
+						modalBody.innerHTML = recordDetail;
+					});
+			})
+		})
 	})
 	.catch(err => console.log('Request Failed', err));
+
+
+
 
 
 function showTableRecord(record, num) {
@@ -23,6 +43,9 @@ function showTableRecord(record, num) {
 		<td>${record.jurusan.name}</td>
 		<td>
 			<button
+				class="btn waves-effect waves-light btn-warning btn-outline-warning modal-detail-btn" data-recordid="${record.id}" data-toggle="modal" data-target="#recordDetailModal"><i
+					class="icofont icofont-edit-alt"></i>Detail</button>
+			<button
 				class="btn waves-effect waves-light btn-warning btn-outline-warning"><i
 					class="icofont icofont-edit-alt"></i>Edit</button>
 			<button
@@ -31,5 +54,52 @@ function showTableRecord(record, num) {
 
 		</td>
 	</tr>
+	`
+}
+
+function showRecordDetail(detail) {
+	return `
+	<div class="container-fluid">
+		<div class="form-group">
+			<label for="full_name">Name</label>
+			<input type="full_name" value="${detail.full_name}" class="form-control form-control-sm" id="full_name" disabled>
+		</div>
+		<div class="form-group">
+			<label for="gender">Gender</label>
+			<input type="gender" value="${detail.gender}" class="form-control form-control-sm" id="gender" disabled>
+		</div>
+		<div class="form-group">
+			<label for="place_of_birth">Place of Birth</label>
+			<input type="place_of_birth" value="${detail.place_of_birth}" class="form-control form-control-sm" id="place_of_birth" disabled>
+		</div>
+		<div class="form-group">
+			<label for="date_of_birth">Date of Birth</label>
+			<input type="text" value="${detail.date_of_birth}" class="form-control form-control-sm date-format" id="date_of_birth" disabled>
+		</div>
+		<div class="form-group">
+			<label for="city_origin">City origin</label>
+			<input type="city_origin" value="${detail.city_origin}" class="form-control form-control-sm" id="city_origin" disabled>
+		</div>
+		<div class="form-group">
+			<label for="kelas_id">Kelas</label>
+			<input type="kelas_id" value="${detail.kelas.name}" class="form-control form-control-sm" id="kelas_id" disabled>
+		</div>
+		<div class="form-group">
+			<label for="kelas_id">Kelas</label>
+			<input type="kelas_id" value="${detail.prodi.name}" class="form-control form-control-sm" id="kelas_id" disabled>
+		</div>
+		<div class="form-group">
+			<label for="kelas_id">Kelas</label>
+			<input type="kelas_id" value="${detail.jurusan.name}" class="form-control form-control-sm" id="kelas_id" disabled>
+		</div>
+		<div class="form-group">
+			<label for="confirmed_date">Confirmed Date</label>
+			<input type="text" value="${detail.confirmed_date}" class="form-control form-control-sm date-format" id="confirmed_date" disabled>
+		</div>
+		<div class="form-group">
+			<label for="status">Status</label>
+			<input type="text" value="${detail.status}" class="form-control form-control-sm date-format" id="confirmed_date" disabled>
+		</div>
+	</div>
 	`
 }
